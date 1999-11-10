2cc8a5bd99dc0c5d374030f0da178e0c03bea89a
mikael <mikael@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
1999-11-10 14:34:56 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@11 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- smsc.c
+++ smsc.c
@@ -559,8 +559,8 @@ SMSCenter *smsc_open(ConfigGroup *grp) {
 	    break;
 
 	case SMSC_TYPE_SMPP_IP:
-	    if (host == NULL || port == NULL || username == NULL || 
-		password == NULL)
+	    if (host == NULL || port == NULL || 
+	    	smpp_system_id == NULL || password == NULL)
 		error(0, "Required field missing for SMPP center.");
 	    else
 		smsc = smpp_open(host, portno, smpp_system_id,
