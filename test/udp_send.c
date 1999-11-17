51e7007b188030419c7c8db14ceb1c0b779fd83b
mikael <mikael@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
1999-11-17 12:54:48 +0000

    udp send tool
    git-svn-id: https://svn.kannel.org/gateway/trunk@93 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

---
+++ test/udp_send.c
@@ -0,0 +1,69 @@
+/*
+ * UDP send test
+ * MG for WAPIT 
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "wapitlib.h"
+
+int main(int argc, char *argv[]) {
+
+	struct sockaddr_in cliaddr, servaddr;
+	struct hostent *hostinfo;
+	socklen_t clilen;
+	int fd;
+
+	if(argc < 3) {
+		printf("usage...\n");
+		exit(1);
+	}
+
+	for(fd=1;fd<argc;fd++) {
+		debug(0, "argv[%i] = <%s>", fd, argv[fd]);
+	}
+
+        fd = socket(PF_INET, SOCK_DGRAM, 0);
+        /* Initialize the sockets. */
+        memset(&servaddr, 0, sizeof(struct sockaddr_in));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(32323);
+
+        while( bind(fd, &servaddr, sizeof(servaddr)) != 0 ) {
+                error(errno, "Could not bind to UDP port <%i>.",
+                        ntohs(servaddr.sin_port));
+                sleep(1);
+        }
+
+	memset(&cliaddr, 0, sizeof(struct sockaddr_in));
+
+	hostinfo = gethostbyname(argv[1]);
+	if (hostinfo == NULL) goto error;
+
+	clilen = sizeof(cliaddr);
+        cliaddr.sin_family = AF_INET;
+        cliaddr.sin_port = htons(atoi(argv[2]));
+        cliaddr.sin_addr = *(struct in_addr *) hostinfo->h_addr;
+
+	sendto(fd, argv[3], strlen(argv[3]), 0, &cliaddr, clilen);
+
+	close(fd);
+
+	return 0;
+error:
+	close(fd);
+	error(errno, "csdr_get_message: could not send UDP datagram");
+	return -1;
+}
+
