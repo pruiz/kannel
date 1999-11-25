39f4fe082a30803fac5f454325a2a5baf5cd3b48
rpr <rpr@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
1999-11-25 17:06:01 +0000

    days
    git-svn-id: https://svn.kannel.org/gateway/trunk@178 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/bearerbox.c
+++ gw/bearerbox.c
@@ -1264,10 +1264,10 @@ static void *http_request_thread(void *arg)
 	t = time(NULL) - start_time;
 	
 	sprintf(answer,
-		"<pre>%s (Time %dh %dm %ds)\n\nQUEUE STATUS:\n%s\n\nTHREAD STATUS:\n%s</pre>",
+		"<pre>%s (Time %dd %dh %dm %ds)\n\nQUEUE STATUS:\n%s\n\nTHREAD STATUS:\n%s</pre>",
 		(bbox->abort_program > 0) ? "Gateway is going down..." : 
 		(bbox->suspended > 0) ? "Gateway is currently suspended" :
-		"Gateway is running", t/3600, t / 60 % 60, t % 60,
+		"Gateway is running", t/3600/24, t/3600 % 24, t/60 % 60, t % 60,
 		buf, buf2);
     } else {
 
