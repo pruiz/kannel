5be931ca60f61a01fd9d62c77f6c39767026f803
liw <liw@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-01-25 15:14:41 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@373 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gwlib/thread.c
+++ gwlib/thread.c
@@ -89,15 +89,32 @@ int mutex_try_lock(Mutex *mutex)
 {
     int ret;
     
+    /* Let's try to lock it. */
     ret = pthread_mutex_trylock(&mutex->mutex);
+
     if (ret == EBUSY)
-	ret = -1;
-    else if (ret != 0)
+	ret = -1;	/* Oops, didn't succeed, someone else locked it. */
+    else if (ret != 0) {
+	/* Oops, some real error occured. The only known case where this
+	   happens is when a mutex object is initialized badly or not
+	   at all. In that case, we're stupid and don't deserve to live. */
         panic(errno, "mutex_try_lock: Mutex failure!");
-    else if (pthread_equal(mutex->owner, pthread_self()))
-        ret = -1;  /* Linux pthread_mutex_trylock doesn't work, I think. */
-    else
+    } else if (pthread_equal(mutex->owner, pthread_self())) {
+	/* The locking succeeded, but the thread system may allow the locking
+	   thread to lock it a second time (Linux seems to do this, even
+	   though the documentation says it shouldn't). We don't want that.
+	   Thus, we keep track of the thread that locked the the mutex and
+	   if we succeed in locking and the locker is us, then we unlock
+	   things and pretend we never succeeded in the first place. The
+	   unlocking is needed because the number of _lock or _trylock
+	   operations and the number of _unlock operations must match. */
+        ret = -1;
+	if (pthread_mutex_unlock(&mutex->mutex) != 0)
+	    panic(errno, "pthread_mutex_unlock failed");
+    } else {
+	/* Hey, it's ours! Let's remember that... */
         mutex->owner = pthread_self();
+    }
     return ret;
 }
 
