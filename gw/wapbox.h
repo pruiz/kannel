b45c54b6127fc70b118f42d022be99bd21b7f088
syvanen <syvanen@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-03-28 11:16:40 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@640 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/wapbox.h
+++ gw/wapbox.h
@@ -8,6 +8,14 @@
 
 #include "msg.h"
 
+/*
+ * Shortest timer tick (in seconds, being shortest defined time amongst 
+ * protocol timers) is currently defined. 
+ */
+
+#define WB_DEFAULT_TIMER_TICK 1
+
+void init_queue(void);
 void put_msg_in_queue(Msg *msg);
 Msg *remove_msg_from_queue(void);
 
