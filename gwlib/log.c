0cbb20097686a0a44660059e07fc56d9488f61f0
liw <liw@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-04-15 14:32:39 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@757 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gwlib/log.c
+++ gwlib/log.c
@@ -117,7 +117,11 @@ static void format(char *buf, int level, const char *place, int e,
 	
 	p = prefix;
 	time(&t);
+#if LOG_TIMESTAMP_LOCALTIME
+	tm = localtime(&t);
+#else
 	tm = gmtime(&t);
+#endif
 	sprintf(p, "%04d-%02d-%02d %02d:%02d:%02d ",
 		tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
 		tm->tm_hour, tm->tm_min, tm->tm_sec);
