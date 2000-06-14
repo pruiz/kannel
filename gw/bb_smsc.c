a559d6d122dbf5fa0c5a122e58e0d3a54d2b4fad
rpr <rpr@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-06-14 13:19:46 +0000

    perf.
    git-svn-id: https://svn.kannel.org/gateway/trunk@1079 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/bb_smsc.c
+++ gw/bb_smsc.c
@@ -62,6 +62,7 @@ static void sms_receiver(void *arg)
     Msg *msg;
     Smsc *conn = arg;
     int ret;
+    int sleep = 1000;
 
     list_add_producer(flow_threads);
     list_add_producer(incoming_sms);
@@ -100,9 +101,16 @@ static void sms_receiver(void *arg)
 
 	    counter_increase(incoming_sms_counter);
 	    debug("bb.sms", 0, "smsc: new message received");
+	    sleep = 1000;
 	}
-	else
-	    usleep(100000);
+	else {
+	    usleep(sleep);
+	    /* XXX: gradually sleep longer and longer times until something starts to
+	     * happen - this of course reduces response time, but that's better than
+	     * extensive CPU usage when it is not used
+	     */
+	    if (sleep < 1000000) sleep *= 10;
+	}
     }    
     list_remove_producer(incoming_sms);
     list_remove_producer(flow_threads);
