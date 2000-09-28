eb793fc2b09aeb80355a1b64c0668c3a30c15f8b
syvanen <syvanen@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-09-28 16:16:13 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1385 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/wtp_init_state-decl.h
+++ gw/wtp_init_state-decl.h
@@ -1,30 +1,30 @@
 /*
- * wtp_init_state.h: Macro calls for implementing wtp iniator state tables
+ * wtp_init_state.h: Macro calls for implementing wtp initiator state tables
  * See documentation for guidance how to use and update these.
  *
- * Only classes 0 and 1 are implemented. State NULL is called INIATOR_NULL_
+ * Only classes 0 and 1 are implemented. State NULL is called INITIATOR_NULL_
  * STATE. 1 in the action field means that action is unconditional.
  *
  * Class 0 service is here a stateless invoke message (used for disconnection
  * or unconfirmed push).
  *
  * Basic class 1 transaction, without timers, is following:
- *               - iniator sends an invoke message to the responder
+ *               - initiator sends an invoke message to the responder
  *               - responder acknowledges it, with an pdu with tid verification
  *                 off (if it is on, we have a tid verification transaction, 
  *                 see below).
  *
  * Retransmission until acknowledgement is implemented using timers and 
- * retransmission counters. When the iniator sends an invoke it starts a timer.
+ * retransmission counters. When the initiator sends an invoke it starts a timer.
  * When it expires, it resends the packet (either ack or invoke), until counter
  * reaches the maximum value. Then the transaction is aborted.
  *
  * If user acknowledgement is on, timers have different values.
  *
- * When the iniator aborts the transaction, it sends an abort pdu. When the
- * responder does it, the iniator wtp user is indicated.
+ * When the initiator aborts the transaction, it sends an abort pdu. When the
+ * responder does it, the initiator wtp user is indicated.
  *
- * Tid verification in the iniator means answering the question posed by the
+ * Tid verification in the initiator means answering the question posed by the
  * responder: "Have you an outstanding transaction having this tid". If we do
  * not have it, we have already, before feeding the event into the state 
  * machine, sended an abort with reason INVALIDTID. So here we answer to  an
@@ -34,15 +34,15 @@
  * By Aarno Syvänen for Wapit Ltd. 
  */
 
-INIT_STATE_NAME(INIATOR_NULL_STATE)
-INIT_STATE_NAME(INIATOR_RESULT_WAIT)
+INIT_STATE_NAME(INITIATOR_NULL_STATE)
+INIT_STATE_NAME(INITIATOR_RESULT_WAIT)
 
 /*
- * We do not use transaction class 2 here: Server is iniator only when it is 
+ * We do not use transaction class 2 here: Server is initiator only when it is 
  * pushing (class 1 or class 0) or disconnecting (class 0). First and second 
  * rows are similar, with exception of timer period.
  */
-ROW(INIATOR_NULL_STATE,
+ROW(INITIATOR_NULL_STATE,
     TR_Invoke_Req,
     event->u.TR_Invoke_Req.tcl == 1,
     {
@@ -67,15 +67,15 @@ ROW(INIATOR_NULL_STATE,
      }
      init_machine->u_ack = event->u.TR_Invoke_Req.up_flag;
      init_machine->rcr = 0;
-     start_iniator_timer_R(init_machine);
+     start_initiator_timer_R(init_machine);
     }, 
-    INIATOR_RESULT_WAIT)
+    INITIATOR_RESULT_WAIT)
 
 /*
  * No need to turn tidnew flag when sending class 0 message; tid validation is
  * not invoked in this case.
  */
-ROW(INIATOR_NULL_STATE,
+ROW(INITIATOR_NULL_STATE,
     TR_Invoke_Req,
     event->u.TR_Invoke_Req.tcl == 0,
     {
@@ -84,37 +84,37 @@ ROW(INIATOR_NULL_STATE,
      
      init_machine->invoke = wtp_send_invoke(init_machine, event);
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     TR_Abort_Req, 
     1,
     {
      wtp_send_abort(USER, event->u.TR_Abort_Req.abort_reason, 
                     init_machine->tid, init_machine->addr_tuple);
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
 /*
  * Neither we check transaction class here: this can only be acknowledgement of
  * class 1 transaction.
  */
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     RcvAck,
     event->u.RcvAck.tid_ok == 0,
     {
-     stop_iniator_timer(init_machine->timer);
+     stop_initiator_timer(init_machine->timer);
 
      wsp_event = create_tr_invoke_cnf(init_machine);
-     wsp_dispatch_event(wsp_event);     
+     wsp_session_dispatch_event(wsp_event);     
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
 /*
  * This is a positive answer to a tid verification. (Negative one being 
  * already sent by init_machine_find_or_create)
  */
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     RcvAck,
     event->u.RcvAck.tid_ok == 1,
     {
@@ -125,21 +125,21 @@ ROW(INIATOR_RESULT_WAIT,
 
      ++init_machine->rcr;
 
-     start_iniator_timer_R(init_machine);
+     start_initiator_timer_R(init_machine);
     },
-    INIATOR_RESULT_WAIT)
+    INITIATOR_RESULT_WAIT)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     RcvAbort,
     1,
     {
      wsp_event = create_tr_abort_ind(init_machine, 
                  event->u.RcvAbort.abort_reason);
-     wsp_dispatch_event(wsp_event);
+     wsp_session_dispatch_event(wsp_event);
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     RcvErrorPDU,
     1,
     {
@@ -147,43 +147,43 @@ ROW(INIATOR_RESULT_WAIT,
                     init_machine->addr_tuple); 
 
      wsp_event = create_tr_abort_ind(init_machine, PROTOERR);
-     wsp_dispatch_event(wsp_event);
+     wsp_session_dispatch_event(wsp_event);
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     TimerTO_R,
     init_machine->rcr < MAX_RCR && !init_machine->tidok_sent,
     {
       ++init_machine->rcr;
 
-     start_iniator_timer_R(init_machine);
+     start_initiator_timer_R(init_machine);
 
      wtp_resend(init_machine->invoke, init_machine->rid);
     },
-    INIATOR_RESULT_WAIT)
+    INITIATOR_RESULT_WAIT)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     TimerTO_R,
     init_machine->rcr < MAX_RCR && init_machine->tidok_sent,
     {
      ++init_machine->rcr;
 
-     start_iniator_timer_R(init_machine);
+     start_initiator_timer_R(init_machine);
 
      wtp_send_ack(TID_VERIFICATION, init_machine->tidok_sent, 
                   init_machine->tid, init_machine->addr_tuple);
     },
-    INIATOR_RESULT_WAIT)
+    INITIATOR_RESULT_WAIT)
 
-ROW(INIATOR_RESULT_WAIT,
+ROW(INITIATOR_RESULT_WAIT,
     TimerTO_R,
     init_machine->rcr == MAX_RCR, 
     {
      wsp_event = create_tr_abort_ind(init_machine, NORESPONSE);
-     wsp_dispatch_event(wsp_event);
+     wsp_session_dispatch_event(wsp_event);
     },
-    INIATOR_NULL_STATE)
+    INITIATOR_NULL_STATE)
 
 #undef ROW
 #undef INIT_STATE_NAME
