fdfb8d3df1c59f581ae6353797bcb5c79cc3cd93
dark <dark@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-10-12 12:31:37 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1482 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gwlib/gwassert.h
+++ gwlib/gwassert.h
@@ -22,12 +22,12 @@
 #else
 #define gw_assert(expr) \
 	((void) ((expr) ? 0 : \
-		  panic(0, "%s:%ld: %s: Assertion `%s' failed.", \
-			__FILE__, (long) __LINE__, __FUNCTION__, #expr), 0))
+		  (panic(0, "%s:%ld: %s: Assertion `%s' failed.", \
+			__FILE__, (long) __LINE__, __FUNCTION__, #expr), 0)))
 #define gw_assert_place(expr, file, lineno, func) \
 	((void) ((expr) ? 0 : \
-		  panic(0, "%s:%ld: %s: Assertion `%s' failed. " \
+		  (panic(0, "%s:%ld: %s: Assertion `%s' failed. " \
 		           "(Called from %s:%ld:%s.)", \
 			      __FILE__, (long) __LINE__, __FUNCTION__, \
-			      #expr, (file), (long) (lineno), (func)), 0))
+			      #expr, (file), (long) (lineno), (func)), 0)))
 #endif
