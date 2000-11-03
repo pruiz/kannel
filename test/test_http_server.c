ac4f037933bf966e736ddc24a7b98aec0f82cd3d
liw <liw@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-11-03 12:10:55 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1602 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- test/test_http_server.c
+++ test/test_http_server.c
@@ -12,125 +12,124 @@
 #include "gwlib/gwlib.h"
 #include "gwlib/http.h"
 
-static void client_thread(void *arg) {
-	HTTPSocket *client_socket;
-	Octstr *body, *url;
-	List *headers, *resph, *cgivars;
-	int ret;
-	HTTPCGIVar *v;
-	
-	client_socket = arg;
-
-	for (;;) {
-		ret = http_server_get_request(client_socket, &url, &headers, 
-				&body, &cgivars);
-		if (ret == -1) {
-			error(0, "http_server_get_request failed");
-			goto error;
-		}
-		if (ret == 0)
-			break;
-		debug("test.http", 0, "Request for <%s>", 
-			octstr_get_cstr(url));
-		while ((v = list_extract_first(cgivars)) != NULL) {
-			debug("test.http", 0, "Var: <%s>=<%s>",
-				octstr_get_cstr(v->name), 
-				octstr_get_cstr(v->value));
-			octstr_destroy(v->name);
-			octstr_destroy(v->value);
-			gw_free(v);
-		}
-		list_destroy(cgivars, NULL);
-
-		octstr_destroy(url);
-		octstr_destroy(body);
-		list_destroy(headers, octstr_destroy_item);
-
-		resph = list_create();
-		list_append(resph, octstr_create("Content-Type: text/plain; "
-						 "charset=\"UTF-8\""));
-		body = octstr_create("hello, world\n");
-		ret = http_server_send_reply(client_socket, HTTP_OK, 
-			resph, body);
-
-		list_destroy(resph, octstr_destroy_item);
-		octstr_destroy(body);
-
-		if (ret == -1) {
-			error(0, "http_server_send_reply failed");
-			goto error;
-		}
+static void client_thread(void *arg) 
+{
+    HTTPSocket *client_socket;
+    Octstr *body, *url;
+    List *headers, *resph, *cgivars;
+    int ret;
+    HTTPCGIVar *v;
+    
+    client_socket = arg;
+    
+    for (;;) {
+	ret = http_server_get_request(client_socket, &url, &headers, 
+				      &body, &cgivars);
+	if (ret == -1) {
+	    error(0, "http_server_get_request failed");
+	    goto error;
 	}
+	if (ret == 0)
+	    break;
+	debug("test.http", 0, "Request for <%s>", octstr_get_cstr(url));
+	while ((v = list_extract_first(cgivars)) != NULL) {
+	    debug("test.http", 0, "Var: <%s>=<%s>",
+		  octstr_get_cstr(v->name), 
+		  octstr_get_cstr(v->value));
+	    octstr_destroy(v->name);
+	    octstr_destroy(v->value);
+	    gw_free(v);
+	}
+	list_destroy(cgivars, NULL);
+    
+	octstr_destroy(url);
+	octstr_destroy(body);
+	list_destroy(headers, octstr_destroy_item);
+    
+	resph = list_create();
+	list_append(resph, 
+	    	    octstr_create("Content-Type: text/plain; "
+		    	    	  "charset=\"UTF-8\""));
+	body = octstr_create("hello, world\n");
+	ret = http_server_send_reply(client_socket, HTTP_OK, resph, body);
+    
+	list_destroy(resph, octstr_destroy_item);
+	octstr_destroy(body);
+    
+	if (ret == -1) {
+	    error(0, "http_server_send_reply failed");
+	    goto error;
+	}
+    }
 
-
-	info(0, "Done with client.");
+    info(0, "Done with client.");
 error:
-	http_server_close_client(client_socket);
+    http_server_close_client(client_socket);
 }
 
 static void help(void) {
-	info(0, "Usage: test_http_server [-p port]\n");
+    info(0, "Usage: test_http_server [-p port]\n");
 }
 
 static void sigterm(int signo) {
-    	exit(0);
+    exit(0);
 }
 
 int main(int argc, char **argv) {
-	int opt, port, use_threads;
-	HTTPSocket *httpd_socket, *client_socket;
-	struct sigaction act;
-	
-	gwlib_init();
+    int opt, port, use_threads;
+    HTTPSocket *httpd_socket, *client_socket;
+    struct sigaction act;
 
-    	act.sa_handler = sigterm;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-    	sigaction(SIGTERM, &act, NULL);
+    gwlib_init();
 
-	port = 8080;
-	use_threads = 0;
+    act.sa_handler = sigterm;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGTERM, &act, NULL);
 
-	while ((opt = getopt(argc, argv, "hv:p:t")) != EOF) {
-		switch (opt) {
-		case 'v':
-			set_output_level(atoi(optarg));
-			break;
+    port = 8080;
+    use_threads = 0;
 
-		case 'h':
-			help();
-			exit(0);
-			
-		case 'p':
-			port = atoi(optarg);
-			break;
-		
-		case 't':
-			use_threads = 1;
-			break;
+    while ((opt = getopt(argc, argv, "hv:p:t")) != EOF) {
+	switch (opt) {
+	case 'v':
+	    set_output_level(atoi(optarg));
+	    break;
 
-		case '?':
-		default:
-			error(0, "Invalid option %c", opt);
-			help();
-			panic(0, "Stopping.");
-		}
+	case 'h':
+	    help();
+	    exit(0);
+
+	case 'p':
+	    port = atoi(optarg);
+	    break;
+
+	case 't':
+	    use_threads = 1;
+	    break;
+
+	case '?':
+	default:
+	    error(0, "Invalid option %c", opt);
+	    help();
+	    panic(0, "Stopping.");
 	}
+    }
 
-	httpd_socket = http_server_open(port);
-	if (httpd_socket == NULL)
-		panic(0, "http_server_open failed");
-	for (;;) {
-		client_socket = http_server_accept_client(httpd_socket);
-		if (client_socket == NULL)
-			panic(0, "http_server_accept_client failed");
-		if (use_threads)
-			gwthread_create(client_thread, client_socket);
-		else
-			client_thread(client_socket);
-	}
-	http_server_close(httpd_socket);
-	
-	gwlib_shutdown();
-	return 0;
+    httpd_socket = http_server_open(port);
+    if (httpd_socket == NULL)
+	panic(0, "http_server_open failed");
+    for (;;) {
+	client_socket = http_server_accept_client(httpd_socket);
+	if (client_socket == NULL)
+	    panic(0, "http_server_accept_client failed");
+	if (use_threads)
+	    gwthread_create(client_thread, client_socket);
+	else
+	    client_thread(client_socket);
+    }
+    http_server_close(httpd_socket);
+
+    gwlib_shutdown();
+    return 0;
 }
