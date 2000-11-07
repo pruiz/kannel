96f8668d71221b41ce0f62dbc80675c9680ea90d
liw <liw@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-11-07 13:00:45 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1620 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

---
+++ checks/check_fakesmsc.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Use `test/fakesmsc' to test the bearerbox and the smsbox.
+
+set -e
+
+times=10
+interval=0.001
+loglevel=0
+
+test/fakesmsc -i $interval -m $times '123 234 www www.kannel.org' \
+    > check_fakesmsc.log 2>&1 &
+
+gw/bearerbox -v $loglevel gw/smskannel.conf > check_fakesmsc_bb.log 2>&1 &
+bbpid=$!
+
+gw/smsbox -v $loglevel gw/smskannel.conf > check_fakesmsc_sms.log 2>&1 &
+
+running=yes
+while [ $running = yes ]
+do
+    sleep 1
+    if grep "got message $times" check_fakesmsc.log >/dev/null
+    then
+    	running=no
+    fi
+done
+
+kill -INT $bbpid
+wait
+
+if grep 'WARNING:|ERROR:|PANIC:' check_fakesmsc*.log >/dev/null
+then
+	echo check_fakesmsc.sh failed 1>&2
+	echo See check_fakesmsc*.log for info 1>&2
+	exit 1
+fi
+
+rm check_fakesmsc*.log
