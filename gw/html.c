bb60af7cfd4d795a32b403753d75c32e9ca624be
dark <dark@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-11-21 17:48:59 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1692 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/html.c
+++ gw/html.c
@@ -16,246 +16,252 @@
 
 
 /* Is there a comment beginning at offset `pos'? */
-static int html_comment_begins(Octstr *html, long pos) {
-	char buf[10];
-	
-	octstr_get_many_chars(buf, html, pos, 4);
-	buf[5] = '\0';
-	return strcmp(buf, "<!--") == 0;
+static int html_comment_begins(Octstr *html, long pos)
+{
+    char buf[10];
+
+    octstr_get_many_chars(buf, html, pos, 4);
+    buf[5] = '\0';
+    return strcmp(buf, "<!--") == 0;
 }
 
 
 /* Skip a comment in HTML. */
-static void skip_html_comment(Octstr *html, long *pos) {
-	long i;
+static void skip_html_comment(Octstr *html, long *pos)
+{
+    long i;
 
-	*pos += 4;	/* Skip "<!--" at beginning of comment. */
-	i = octstr_search(html, octstr_create_immutable("-->"), *pos);
-	if (i == -1)
-		*pos = octstr_len(html);
-	else
-		*pos = i;
+    *pos += 4; 	/* Skip "<!--" at beginning of comment. */
+    i = octstr_search(html, octstr_create_immutable("-->"), *pos);
+    if (i == -1)
+        *pos = octstr_len(html);
+    else
+        *pos = i;
 }
 
 
 /* Skip a beginning of ending tag in HTML, including any attributes. */
-static void skip_html_tag(Octstr *html, long *pos) {
-	long i, len;
-	int c;
+static void skip_html_tag(Octstr *html, long *pos)
+{
+    long i, len;
+    int c;
 
-	/* Skip leading '<'. */
-	++(*pos);
+    /* Skip leading '<'. */
+    ++(*pos);
 
-	/* Skip name of tag and attributes with values. */
-	len = octstr_len(html);
-	while (*pos < len && (c = octstr_get_char(html, *pos)) != '>') {
-		if (c == '"' || c == '\'') {
-			i = octstr_search_char(html, c, *pos + 1);
-			if (i == -1)
-				*pos = len;
-			else
-				*pos = i + 1;
-		} else
-			++(*pos);
-	}
-	
-	/* Skip trailing '>' if it is there. */
-	if (octstr_get_char(html, *pos) == '>')
-		++(*pos);
+    /* Skip name of tag and attributes with values. */
+    len = octstr_len(html);
+    while (*pos < len && (c = octstr_get_char(html, *pos)) != '>') {
+        if (c == '"' || c == '\'') {
+            i = octstr_search_char(html, c, *pos + 1);
+            if (i == -1)
+                *pos = len;
+            else
+                *pos = i + 1;
+        } else
+            ++(*pos);
+    }
+
+    /* Skip trailing '>' if it is there. */
+    if (octstr_get_char(html, *pos) == '>')
+        ++(*pos);
 }
 
 
 /* Convert an HTML entity into a single character and advance `*html' past
    the entity. */
-static void convert_html_entity(Octstr *sms, Octstr *html, long *pos) {
-	static struct {
-		char *entity;
-		int character;
-	} tab[] = {
-		{ "&amp;", '&' },
-		{ "&lt;", '<' },
-		{ "&gt;", '>' },
+static void convert_html_entity(Octstr *sms, Octstr *html, long *pos)
+{
+    static struct {
+        char *entity;
+        int character;
+    }
+    tab[] = {
+        { "&amp;", '&' },
+        { "&lt;", '<' },
+        { "&gt;", '>' },
 
-		/* The following is copied from 
+        /* The following is copied from
 
-			http://www.hut.fi/~jkorpela/HTML3.2/latin1.html
-		
-		   by Jukka Korpela. Hand and script edited to form this
-		   table. */
+        	http://www.hut.fi/~jkorpela/HTML3.2/latin1.html
 
-		{ "&nbsp;", ' ' },
-		{ "&iexcl;", 161 },
-		{ "&cent;", 162 },
-		{ "&pound;", 163 },
-		{ "&curren;", 164 },
-		{ "&yen;", 165 },
-		{ "&brvbar;", 166 },
-		{ "&sect;", 167 },
-		{ "&uml;", 168 },
-		{ "&copy;", 169 },
-		{ "&ordf;", 170 },
-		{ "&laquo;", 171 },
-		{ "&not;", 172 },
-		{ "&shy;", 173 },
-		{ "&reg;", 174 },
-		{ "&macr;", 175 },
-		{ "&deg;", 176 },
-		{ "&plusmn;", 177 },
-		{ "&sup2;", 178 },
-		{ "&sup3;", 179 },
-		{ "&acute;", 180 },
-		{ "&micro;", 181 },
-		{ "&para;", 182 },
-		{ "&middot;", 183 },
-		{ "&cedil;", 184 },
-		{ "&sup1;", 185 },
-		{ "&ordm;", 186 },
-		{ "&raquo;", 187 },
-		{ "&frac14;", 188 },
-		{ "&frac12;", 189 },
-		{ "&frac34;", 190 },
-		{ "&iquest;", 191 },
-		{ "&Agrave;", 192 },
-		{ "&Aacute;", 193 },
-		{ "&Acirc;", 194 },
-		{ "&Atilde;", 195 },
-		{ "&Auml;", 196 },
-		{ "&Aring;", 197 },
-		{ "&AElig;", 198 },
-		{ "&Ccedil;", 199 },
-		{ "&Egrave;", 200 },
-		{ "&Eacute;", 201 },
-		{ "&Ecirc;", 202 },
-		{ "&Euml;", 203 },
-		{ "&Igrave;", 204 },
-		{ "&Iacute;", 205 },
-		{ "&Icirc;", 206 },
-		{ "&Iuml;", 207 },
-		{ "&ETH;", 208 },
-		{ "&Ntilde;", 209 },
-		{ "&Ograve;", 210 },
-		{ "&Oacute;", 211 },
-		{ "&Ocirc;", 212 },
-		{ "&Otilde;", 213 },
-		{ "&Ouml;", 214 },
-		{ "&times;", 215 },
-		{ "&Oslash;", 216 },
-		{ "&Ugrave;", 217 },
-		{ "&Uacute;", 218 },
-		{ "&Ucirc;", 219 },
-		{ "&Uuml;", 220 },
-		{ "&Yacute;", 221 },
-		{ "&THORN;", 222 },
-		{ "&szlig;", 223 },
-		{ "&agrave;", 224 },
-		{ "&aacute;", 225 },
-		{ "&acirc;", 226 },
-		{ "&atilde;", 227 },
-		{ "&auml;", 228 },
-		{ "&aring;", 229 },
-		{ "&aelig;", 230 },
-		{ "&ccedil;", 231 },
-		{ "&egrave;", 232 },
-		{ "&eacute;", 233 },
-		{ "&ecirc;", 234 },
-		{ "&euml;", 235 },
-		{ "&igrave;", 236 },
-		{ "&iacute;", 237 },
-		{ "&icirc;", 238 },
-		{ "&iuml;", 239 },
-		{ "&eth;", 240 },
-		{ "&ntilde;", 241 },
-		{ "&ograve;", 242 },
-		{ "&oacute;", 243 },
-		{ "&ocirc;", 244 },
-		{ "&otilde;", 245 },
-		{ "&ouml;", 246 },
-		{ "&divide;", 247 },
-		{ "&oslash;", 248 },
-		{ "&ugrave;", 249 },
-		{ "&uacute;", 250 },
-		{ "&ucirc;", 251 },
-		{ "&uuml;", 252 },
-		{ "&yacute;", 253 },
-		{ "&thorn;", 254 },
-		{ "&yuml;", 255 },
-	};
-	int num_tab = sizeof(tab) / sizeof(tab[0]);
-	long i, code;
-	size_t len;
-	char buf[1024];
-	
-	if (octstr_get_char(html, (*pos) + 1) == '#') {
-		i = octstr_parse_long(&code, html, *pos, 10);
-		if (i > 0) {
-			if (code < 256)
-				octstr_append_char(sms, code);
-			*pos += i + 1;
-			if (octstr_get_char(html, *pos) == ';')
-				++(*pos);
-		}
-	} else {
-		for (i = 0; i < num_tab; ++i) {
-			len = strlen(tab[i].entity);
-			octstr_get_many_chars(buf, html, *pos, len);
-			buf[len] = '\0';
-			if (strcmp(buf, tab[i].entity) == 0) {
-				*pos += len;
-				octstr_append_char(sms, tab[i].character);
-				break;
-			}
-		}
-		if (i == num_tab) {
-			++(*pos);
-			octstr_append_char(sms, '&');
-		}
-	}
+           by Jukka Korpela. Hand and script edited to form this
+           table. */
+
+        { "&nbsp;", ' ' },
+        { "&iexcl;", 161 },
+        { "&cent;", 162 },
+        { "&pound;", 163 },
+        { "&curren;", 164 },
+        { "&yen;", 165 },
+        { "&brvbar;", 166 },
+        { "&sect;", 167 },
+        { "&uml;", 168 },
+        { "&copy;", 169 },
+        { "&ordf;", 170 },
+        { "&laquo;", 171 },
+        { "&not;", 172 },
+        { "&shy;", 173 },
+        { "&reg;", 174 },
+        { "&macr;", 175 },
+        { "&deg;", 176 },
+        { "&plusmn;", 177 },
+        { "&sup2;", 178 },
+        { "&sup3;", 179 },
+        { "&acute;", 180 },
+        { "&micro;", 181 },
+        { "&para;", 182 },
+        { "&middot;", 183 },
+        { "&cedil;", 184 },
+        { "&sup1;", 185 },
+        { "&ordm;", 186 },
+        { "&raquo;", 187 },
+        { "&frac14;", 188 },
+        { "&frac12;", 189 },
+        { "&frac34;", 190 },
+        { "&iquest;", 191 },
+        { "&Agrave;", 192 },
+        { "&Aacute;", 193 },
+        { "&Acirc;", 194 },
+        { "&Atilde;", 195 },
+        { "&Auml;", 196 },
+        { "&Aring;", 197 },
+        { "&AElig;", 198 },
+        { "&Ccedil;", 199 },
+        { "&Egrave;", 200 },
+        { "&Eacute;", 201 },
+        { "&Ecirc;", 202 },
+        { "&Euml;", 203 },
+        { "&Igrave;", 204 },
+        { "&Iacute;", 205 },
+        { "&Icirc;", 206 },
+        { "&Iuml;", 207 },
+        { "&ETH;", 208 },
+        { "&Ntilde;", 209 },
+        { "&Ograve;", 210 },
+        { "&Oacute;", 211 },
+        { "&Ocirc;", 212 },
+        { "&Otilde;", 213 },
+        { "&Ouml;", 214 },
+        { "&times;", 215 },
+        { "&Oslash;", 216 },
+        { "&Ugrave;", 217 },
+        { "&Uacute;", 218 },
+        { "&Ucirc;", 219 },
+        { "&Uuml;", 220 },
+        { "&Yacute;", 221 },
+        { "&THORN;", 222 },
+        { "&szlig;", 223 },
+        { "&agrave;", 224 },
+        { "&aacute;", 225 },
+        { "&acirc;", 226 },
+        { "&atilde;", 227 },
+        { "&auml;", 228 },
+        { "&aring;", 229 },
+        { "&aelig;", 230 },
+        { "&ccedil;", 231 },
+        { "&egrave;", 232 },
+        { "&eacute;", 233 },
+        { "&ecirc;", 234 },
+        { "&euml;", 235 },
+        { "&igrave;", 236 },
+        { "&iacute;", 237 },
+        { "&icirc;", 238 },
+        { "&iuml;", 239 },
+        { "&eth;", 240 },
+        { "&ntilde;", 241 },
+        { "&ograve;", 242 },
+        { "&oacute;", 243 },
+        { "&ocirc;", 244 },
+        { "&otilde;", 245 },
+        { "&ouml;", 246 },
+        { "&divide;", 247 },
+        { "&oslash;", 248 },
+        { "&ugrave;", 249 },
+        { "&uacute;", 250 },
+        { "&ucirc;", 251 },
+        { "&uuml;", 252 },
+        { "&yacute;", 253 },
+        { "&thorn;", 254 },
+        { "&yuml;", 255 },
+    };
+    int num_tab = sizeof(tab) / sizeof(tab[0]);
+    long i, code;
+    size_t len;
+    char buf[1024];
+
+    if (octstr_get_char(html, (*pos) + 1) == '#') {
+        i = octstr_parse_long(&code, html, *pos, 10);
+        if (i > 0) {
+            if (code < 256)
+                octstr_append_char(sms, code);
+            *pos += i + 1;
+            if (octstr_get_char(html, *pos) == ';')
+                ++(*pos);
+        }
+    } else {
+        for (i = 0; i < num_tab; ++i) {
+            len = strlen(tab[i].entity);
+            octstr_get_many_chars(buf, html, *pos, len);
+            buf[len] = '\0';
+            if (strcmp(buf, tab[i].entity) == 0) {
+                *pos += len;
+                octstr_append_char(sms, tab[i].character);
+                break;
+            }
+        }
+        if (i == num_tab) {
+            ++(*pos);
+            octstr_append_char(sms, '&');
+        }
+    }
 }
 
 
-Octstr *html_to_sms(Octstr *html) {
-	long i, len;
-	int c;
-	Octstr *sms;
-	
-	sms = octstr_create("");
-	len = octstr_len(html);
-	i = 0;
-	while (i < len) {
-		c = octstr_get_char(html, i);
-		switch (c) {
-		case '<':
-			if (html_comment_begins(html, i))
-				skip_html_comment(html, &i);
-			else
-				skip_html_tag(html, &i);
-			break;
-		case '&':
-			convert_html_entity(sms, html, &i);
-			break;
-		default:
-			octstr_append_char(sms, c);
-			++i;
-			break;
-		}
-	}
-	octstr_shrink_blanks(sms);
-	octstr_strip_blanks(sms);
-	return sms;
+Octstr *html_to_sms(Octstr *html)
+{
+    long i, len;
+    int c;
+    Octstr *sms;
+
+    sms = octstr_create("");
+    len = octstr_len(html);
+    i = 0;
+    while (i < len) {
+        c = octstr_get_char(html, i);
+        switch (c) {
+        case '<':
+            if (html_comment_begins(html, i))
+                skip_html_comment(html, &i);
+            else
+                skip_html_tag(html, &i);
+            break;
+        case '&':
+            convert_html_entity(sms, html, &i);
+            break;
+        default:
+            octstr_append_char(sms, c);
+            ++i;
+            break;
+        }
+    }
+    octstr_shrink_blanks(sms);
+    octstr_strip_blanks(sms);
+    return sms;
 }
 
 
 Octstr *html_strip_prefix_and_suffix(Octstr *html, char *prefix, char *suffix)
 {
-	char *p, *q, *data;
+    char *p, *q, *data;
 
-	data = octstr_get_cstr(html);
-	p = str_case_str(data, prefix);
-	if (p == NULL)
-		return octstr_duplicate(html);
-	p += strlen(prefix);
-	q = str_case_str(p, suffix);
-	if (q == NULL)
-		return octstr_duplicate(html);
-	return octstr_copy(html, p - data, q - p);
+    data = octstr_get_cstr(html);
+    p = str_case_str(data, prefix);
+    if (p == NULL)
+        return octstr_duplicate(html);
+    p += strlen(prefix);
+    q = str_case_str(p, suffix);
+    if (q == NULL)
+        return octstr_duplicate(html);
+    return octstr_copy(html, p - data, q - p);
 }
--- gw/msg.c
+++ gw/msg.c
@@ -33,136 +33,145 @@ static char *type_as_str(Msg *msg);
  * Implementations of the exported functions.
  */
 
-Msg *msg_create(enum msg_type type) {
-	Msg *msg;
+Msg *msg_create(enum msg_type type)
+{
+    Msg *msg;
 
-	msg = gw_malloc(sizeof(Msg));
+    msg = gw_malloc(sizeof(Msg));
 
-	msg->type = type;
+    msg->type = type;
 
-	#define INTEGER(name) p->name = 0
-	#define OCTSTR(name) p->name = NULL
-	#define MSG(type, stmt) { struct type *p = &msg->type; stmt }
-	#include "msg-decl.h"
+#define INTEGER(name) p->name = 0
+#define OCTSTR(name) p->name = NULL
+#define MSG(type, stmt) { struct type *p = &msg->type; stmt }
+#include "msg-decl.h"
 
-	return msg;
+    return msg;
 }
 
-Msg *msg_duplicate(Msg *msg) {
-	Msg *new;
+Msg *msg_duplicate(Msg *msg)
+{
+    Msg *new;
 
-	new = msg_create(msg->type);
+    new = msg_create(msg->type);
 
-	#define INTEGER(name) p->name = q->name
-	#define OCTSTR(name) \
-		if (q->name == NULL) p->name = NULL; \
-		else p->name = octstr_duplicate(q->name);
-	#define MSG(type, stmt) { \
-		struct type *p = &new->type; \
-		struct type *q = &msg->type; \
-		stmt }
-	#include "msg-decl.h"
+#define INTEGER(name) p->name = q->name
+#define OCTSTR(name) \
+    if (q->name == NULL) p->name = NULL; \
+    else p->name = octstr_duplicate(q->name);
+#define MSG(type, stmt) { \
+    struct type *p = &new->type; \
+    struct type *q = &msg->type; \
+    stmt }
+#include "msg-decl.h"
 
-	return new;
+    return new;
 }
 
-void msg_destroy(Msg *msg) {
-	if (msg == NULL)
-		return;
+void msg_destroy(Msg *msg)
+{
+    if (msg == NULL)
+        return;
 
-	#define INTEGER(name) p->name = 0
-	#define OCTSTR(name) octstr_destroy(p->name)
-	#define MSG(type, stmt) { struct type *p = &msg->type; stmt }
-	#include "msg-decl.h"
+#define INTEGER(name) p->name = 0
+#define OCTSTR(name) octstr_destroy(p->name)
+#define MSG(type, stmt) { struct type *p = &msg->type; stmt }
+#include "msg-decl.h"
 
-	gw_free(msg);
+    gw_free(msg);
 }
 
-void msg_destroy_item(void *msg) {
-	msg_destroy(msg);
+void msg_destroy_item(void *msg)
+{
+    msg_destroy(msg);
 }
 
-void msg_dump(Msg *msg, int level) {
-	debug("gw.msg", 0, "%*sMsg object at %p:", level, "", (void *) msg);
-	debug("gw.msg", 0, "%*s type: %s", level, "", type_as_str(msg));
-	#define INTEGER(name) \
-		debug("gw.msg", 0, "%*s %s.%s: %ld", \
-			level, "", t, #name, (long) p->name)
-	#define OCTSTR(name) \
-		debug("gw.msg", 0, "%*s %s.%s:", level, "", t, #name); \
-		octstr_dump(p->name, level + 1)
-	#define MSG(tt, stmt) \
-		if (tt == msg->type) \
-			{ char *t = #tt; struct tt *p = &msg->tt; stmt }
-	#include "msg-decl.h"
-	debug("gw.msg", 0, "Msg object ends.");
-}
-
-
-enum msg_type msg_type(Msg *msg) {
-	return msg->type;
-}
-
-Octstr *msg_pack(Msg *msg) {
-	Octstr *os;
-
-	os = octstr_create("");
-	append_integer(os, msg->type);
-
-	#define INTEGER(name) append_integer(os, p->name)
-	#define OCTSTR(name) append_string(os, p->name)
-	#define MSG(type, stmt) \
-		case type: { struct type *p = &msg->type; stmt } break;
-	switch (msg->type) {
-		#include "msg-decl.h"
-	default:
-		panic(0, "Internal error: unknown message type %d", 
-		    	 msg->type);
-	}
-
-	prepend_integer(os, octstr_len(os));
-
-	return os;
+void msg_dump(Msg *msg, int level)
+{
+    debug("gw.msg", 0, "%*sMsg object at %p:", level, "", (void *) msg);
+    debug("gw.msg", 0, "%*s type: %s", level, "", type_as_str(msg));
+#define INTEGER(name) \
+    debug("gw.msg", 0, "%*s %s.%s: %ld", level, "", t, #name, (long) p->name)
+#define OCTSTR(name) \
+    debug("gw.msg", 0, "%*s %s.%s:", level, "", t, #name); \
+    octstr_dump(p->name, level + 1)
+#define MSG(tt, stmt) \
+    if (tt == msg->type) \
+        { char *t = #tt; struct tt *p = &msg->tt; stmt }
+#include "msg-decl.h"
+    debug("gw.msg", 0, "Msg object ends.");
 }
 
 
-Msg *msg_unpack(Octstr *os) {
-	Msg *msg;
-	int off;
-	long i;
+enum msg_type msg_type(Msg *msg)
+{
+    return msg->type;
+}
 
-	msg = msg_create(0);
-	if (msg == NULL)
-		goto error;
+Octstr *msg_pack(Msg *msg)
+{
+    Octstr *os;
 
-	off = 0;
+    os = octstr_create("");
+    append_integer(os, msg->type);
 
-	/* Skip length. */
-	if (parse_integer(&i, os, &off) == -1)
-		goto error;
+#define INTEGER(name) append_integer(os, p->name)
+#define OCTSTR(name) append_string(os, p->name)
+#define MSG(type, stmt) \
+    case type: { struct type *p = &msg->type; stmt } break;
 
-	if (parse_integer(&i, os, &off) == -1)
-		goto error;
-	msg->type = i;
+    switch (msg->type) {
+#include "msg-decl.h"
+    default:
+        panic(0, "Internal error: unknown message type %d",
+              msg->type);
+    }
 
-	#define INTEGER(name) \
-		if (parse_integer(&(p->name), os, &off) == -1) goto error
-	#define OCTSTR(name) \
-		if (parse_string(&(p->name), os, &off) == -1) goto error
-	#define MSG(type, stmt) \
-		case type: { struct type *p = &(msg->type); stmt } break;
-	switch (msg->type) {
-		#include "msg-decl.h"
-	default:
-		panic(0, "Internal error: unknown message type: %d", 
-			msg->type);
-	}
+    prepend_integer(os, octstr_len(os));
 
-	return msg;
+    return os;
+}
+
+
+Msg *msg_unpack(Octstr *os)
+{
+    Msg *msg;
+    int off;
+    long i;
+
+    msg = msg_create(0);
+    if (msg == NULL)
+        goto error;
+
+    off = 0;
+
+    /* Skip length. */
+    if (parse_integer(&i, os, &off) == -1)
+        goto error;
+
+    if (parse_integer(&i, os, &off) == -1)
+        goto error;
+    msg->type = i;
+
+#define INTEGER(name) \
+    if (parse_integer(&(p->name), os, &off) == -1) goto error
+#define OCTSTR(name) \
+    if (parse_string(&(p->name), os, &off) == -1) goto error
+#define MSG(type, stmt) \
+    case type: { struct type *p = &(msg->type); stmt } break;
+
+    switch (msg->type) {
+#include "msg-decl.h"
+    default:
+        panic(0, "Internal error: unknown message type: %d",
+              msg->type);
+    }
+
+    return msg;
 
 error:
-	error(errno, "Msg packet was invalid.");
-	return NULL;
+    error(errno, "Msg packet was invalid.");
+    return NULL;
 }
 
 
@@ -171,75 +180,81 @@ error:
  */
 
 
-static void append_integer(Octstr *os, long i) {
-	Octstr *temp;
+static void append_integer(Octstr *os, long i)
+{
+    Octstr *temp;
 
-	i = htonl(i);
-	temp = octstr_create_from_data((char *) &i, sizeof(i));
-	octstr_insert(os, temp, octstr_len(os));
-	octstr_destroy(temp);
+    i = htonl(i);
+    temp = octstr_create_from_data((char *) & i, sizeof(i));
+    octstr_insert(os, temp, octstr_len(os));
+    octstr_destroy(temp);
 }
 
-static void prepend_integer(Octstr *os, long i) {
-	Octstr *temp;
+static void prepend_integer(Octstr *os, long i)
+{
+    Octstr *temp;
 
-	i = htonl(i);
-	temp = octstr_create_from_data((char *) &i, sizeof(i));
-	octstr_insert(os, temp, 0);
-	octstr_destroy(temp);
+    i = htonl(i);
+    temp = octstr_create_from_data((char *) & i, sizeof(i));
+    octstr_insert(os, temp, 0);
+    octstr_destroy(temp);
 }
 
-static void append_string(Octstr *os, Octstr *field) {
-	if (field == NULL)
-		append_integer(os, -1);
-	else {
-		append_integer(os, octstr_len(field));
-		octstr_insert(os, field, octstr_len(os));
-	}
-}
-
-
-static int parse_integer(long *i, Octstr *packed, int *off) {
-	gw_assert(*off >= 0);
-	if ((int) sizeof(long) + *off > octstr_len(packed)) {
-		error(0, "Packet too short while unpacking Msg.");
-		return -1;
-	}
-
-	octstr_get_many_chars((char *) i, packed, *off, sizeof(long));
-	*i = ntohl(*i);
-	*off += sizeof(long);
-	return 0;
+static void append_string(Octstr *os, Octstr *field)
+{
+    if (field == NULL)
+        append_integer(os, -1);
+    else {
+        append_integer(os, octstr_len(field));
+        octstr_insert(os, field, octstr_len(os));
+    }
 }
 
 
-static int parse_string(Octstr **os, Octstr *packed, int *off) {
-	long len;
+static int parse_integer(long *i, Octstr *packed, int *off)
+{
+    gw_assert(*off >= 0);
+    if ((int) sizeof(long) + *off > octstr_len(packed)) {
+        error(0, "Packet too short while unpacking Msg.");
+        return -1;
+    }
 
-	if (parse_integer(&len, packed, off) == -1)
-		return -1;
-
-	if (len == -1) {
-		*os = NULL;
-		return 0;
-	}
-
-	/* XXX check that len is ok */
-
-	*os = octstr_copy(packed, *off, len);
-	if (*os == NULL)
-		return -1;
-	*off += len;
-
-	return 0;
+    octstr_get_many_chars((char *) i, packed, *off, sizeof(long));
+    *i = ntohl(*i);
+    *off += sizeof(long);
+    return 0;
 }
 
 
-static char *type_as_str(Msg *msg) {
-	switch (msg->type) {
-	#define MSG(t, stmt) case t: return #t;
-	#include "msg-decl.h"
-	default:
-		return "unknown type";
-	}
+static int parse_string(Octstr **os, Octstr *packed, int *off)
+{
+    long len;
+
+    if (parse_integer(&len, packed, off) == -1)
+        return -1;
+
+    if (len == -1) {
+        *os = NULL;
+        return 0;
+    }
+
+    /* XXX check that len is ok */
+
+    *os = octstr_copy(packed, *off, len);
+    if (*os == NULL)
+        return -1;
+    *off += len;
+
+    return 0;
+}
+
+
+static char *type_as_str(Msg *msg)
+{
+    switch (msg->type) {
+#define MSG(t, stmt) case t: return #t;
+#include "msg-decl.h"
+    default:
+        return "unknown type";
+    }
 }
