a15beb320b706ab2b2b8a9f6cf0c0faa5dddabb3
dark <dark@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2000-12-15 14:23:59 +0000

    Test new dict_key_count() too.
    git-svn-id: https://svn.kannel.org/gateway/trunk@1808 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- test/test_dict.c
+++ test/test_dict.c
@@ -21,6 +21,10 @@ int main(void)
     dict = dict_create(10, NULL);
     dict_put(dict, foo, bar);
     info(0, "foo gives %s", octstr_get_cstr(dict_get(dict, foo)));
+    if (dict_key_count(dict) == 1)
+	info(0, "there is but one foo.");
+    else
+        error(0, "key count is %ld, should be 1.", dict_key_count(dict));
     dict_destroy(dict);
     gwlib_shutdown();
     return 0;
