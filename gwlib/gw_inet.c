9d027869a2c110e43de0da0163758867e2f8d015
derry <derry@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2001-01-08 16:14:37 +0000

    Fixed stupid error in switch.  It helps if there is a case statement rather than just a label.
    git-svn-id: https://svn.kannel.org/gateway/trunk@1863 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gwlib/gw_inet.c
+++ gwlib/gw_inet.c
@@ -7,45 +7,45 @@
 
 char *gw_inet_ntop(int af, const void *src, char *dst, size_t size)
 {
-    const char* ba; /* ByteAddress - allow us to bytewise address the src.*/
+    const char* ba;  /* ByteAddress - allow us to bytewise address the src.*/
     char* dst_tmp;
     int ret;
 
-    if(src == NULL || dst == NULL || size < 1){
-	    errno = ENOSPC;
-	    return NULL;
-	}
-	
+    if (src == NULL || dst == NULL || size < 1) {
+        errno = ENOSPC;
+        return NULL;
+    }
 
-    switch(af){
-    AF_INET:
-	/* Normal IPv4 address */
-	ba = src;
 
-	dst_tmp = gw_malloc(sizeof(INET_ADDRSTRLEN));
-	if(dst_tmp == NULL){
-	    errno = ENOSPC;
-	    return NULL;
-	}
-	    
-	
-	ret = snprintf(dst_tmp,sizeof(INET_ADDRSTRLEN),"%u.%u.%u.%u",
-	    ba[0], ba[1], ba[2], ba[3]);
-	
-	/* If ret > size then we don't have the space to continue. 
-	 * If ret > sizeof(INET_ADDRSTRLEN) then it is stuffed anyway. */
-	if(ret>size || ret > sizeof(INET_ADDRSTRLEN)){
-	    errno = ENOSPC;
-	    return NULL;
-	}
+    switch (af) {
+    case AF_INET:
+        /* Normal IPv4 address */
+        ba = src;
 
-	strncpy(dst,dst_tmp,size);
-	return dst;
+        dst_tmp = gw_malloc(sizeof(INET_ADDRSTRLEN));
+        if (dst_tmp == NULL) {
+            errno = ENOSPC;
+            return NULL;
+        }
 
-	break;
+
+        ret = snprintf(dst_tmp, sizeof(INET_ADDRSTRLEN), "%u.%u.%u.%u",
+                       ba[0], ba[1], ba[2], ba[3]);
+
+        /* If ret > size then we don't have the space to continue.
+         * If ret > sizeof(INET_ADDRSTRLEN) then it is stuffed anyway. */
+        if (ret > size || ret > sizeof(INET_ADDRSTRLEN)) {
+            errno = ENOSPC;
+            return NULL;
+        }
+
+        strncpy(dst, dst_tmp, size);
+        return dst;
+
+        break;
     default:
-	/* We don't know how to handle this type of address */
-	errno=EAFNOSUPPORT;
-	return NULL;
+        /* We don't know how to handle this type of address */
+        errno = EAFNOSUPPORT;
+        return NULL;
     }
 }
