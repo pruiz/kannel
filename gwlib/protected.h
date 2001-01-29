189f9be13c98dd95af1bc951cf331263489cf7b3
liw <liw@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2001-01-29 13:01:43 +0000

    .
    git-svn-id: https://svn.kannel.org/gateway/trunk@1938 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gwlib/protected.h
+++ gwlib/protected.h
@@ -25,10 +25,20 @@ int gw_gethostbyname(struct hostent *ret, const char *name);
 /*
  * Make it harder to use these by mistake.
  */
+
+#undef localtime
 #define localtime(t) do_not_use_localtime_directly
+
+#undef gmtime
 #define gmtime(t) do_not_use_gmtime_directly
+
+#undef rand
 #define rand() do_not_use_rand_directly
+
+#undef gethostbyname
 #define gethostbyname() do_not_use_gethostbyname_directly
+
+#undef inet_ntoa
 #define inet_ntoa(in) use_gw_netaddr_to_octstr_instead_of_inet_ntoa
 
 
