7c8ed779733414947fe1868d2473e256974de4dd
uau <uau@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2001-02-06 21:35:06 +0000

    Changed %a URL parameter not to mess with whitespace
    git-svn-id: https://svn.kannel.org/gateway/trunk@1970 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/urltrans.c
+++ gw/urltrans.c
@@ -328,13 +328,9 @@ Octstr *urltrans_get_pattern(URLTranslation *t, Msg *request)
 	break;
 
 	case 'a':
-	    for (j = 0; j < num_words; ++j) {
-		enc = octstr_create_urlcoded(list_get(word_list, j));
-		if (j > 0)
-		    octstr_append_char(result, '+');
-		octstr_append(result, enc);
-		octstr_destroy(enc);
-	    }
+	    enc = octstr_create_urlcoded(request->sms.msgdata);
+	    octstr_append(result, enc);
+	    octstr_destroy(enc);
 	    break;
 
 	case 't':
