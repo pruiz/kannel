434975ea41382026654857f109798794010e89e6
derry <derry@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2001-03-07 14:47:58 +0000

    modified the files to name the package with the actual version rather than just assume that it was CVS.
    git-svn-id: https://svn.kannel.org/gateway/trunk@2035 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- solaris/mk-solaris-package.sh
+++ solaris/mk-solaris-package.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+VERSION=`head -1 ../VERSION`
+DATE=`date +%Y-%m-%d`
+
+echo Making WAkannel-${VERSION}-${DATE}-Solaris7-sparc-local.tar.Z
 cd ..
 CFLAGS="-Wall -O2 -Xlinker -rpath -Xlinker /usr/local/lib" ./configure --prefix=./solaris/kannel --with-malloc=native
 make 
 make install
 cd solaris
+sed "s/VERSION_NUM/${VERSION}/" < prototype.tmpl > prototype
 pkgmk -r `pwd` -d .
-tar cvf WAkannel-cvs-Solaris7-sparc-local.tar WAkannel
-compress WAkannel-cvs-Solaris7-sparc-local.tar
+tar cvf WAkannel-${VERSION}-${DATE}-Solaris7-sparc-local.tar WAkannel
+compress WAkannel-${VERSION}-${DATE}-Solaris7-sparc-local.tar
