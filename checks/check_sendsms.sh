d6b16be9369d5b26903fc485d1104faa741b35de
syvanen <syvanen@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2002-04-26 10:23:14 +0000

    testing empty cgi vars
    git-svn-id: https://svn.kannel.org/gateway/trunk@2760 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- checks/check_sendsms.sh
+++ checks/check_sendsms.sh
@@ -3,6 +3,7 @@
 # Use `test/fakesmsc' to test sendsms in smsbox.
 
 set -e
+#set -x
 
 times=10
 interval=0
@@ -27,6 +28,10 @@ gw/smsbox -v $loglevel gw/smskannel.conf > check_sendsms_sms.log 2>&1 &
 
 sleep 2
 
+# All cgivars are OK
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=123&to=234&\
+text=test&username=$username&password=$password"
+
 i=0
 while [ $i -lt $times ]
 do
@@ -35,9 +40,6 @@ do
 done
 
 sleep 5
-
-kill -INT $bbpid
-wait
 
 if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
    [ $times -ne `grep -c 'Got message .*: <123 234 text test>' \
@@ -48,4 +50,92 @@ then
 	exit 1
 fi
 
+# Empty fields: message. This is OK, we must get a canned reply
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=123&to=234&\
+text=&username=$username&password=$password"
+
+test/test_http $url >> check_sendsms.log 2>&1
+
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
+   [ 1 -ne `grep -c '<123 234 text <Empty reply from service provider>' \
+       check_sendsms_smsc.log` ]
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
+#From. This is OK, too: now sendsms port replaces from field
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=&to=234&\
+text=test&username=$username&password=$password"
+
+test/test_http $url >> check_sendsms.log 2>&1
+
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
+   [ 1 -ne `grep -c '<'$sendsmsport' 234 text test>' check_sendsms_smsc.log` ]
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
+#To. Now smsbox must report an error.
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=123&to=&\
+text=&username=$username&password=$password"
+
+test/test_http $url >> check_sendsms.log 2>&1
+
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
+   [ 1 -ne `grep -c 'got empty to cgi variable' check_sendsms_sms.log` ]
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
+#Username. This is an authentication error.
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=123&to=234&\
+text=&username=&password=$password"
+
+test/test_http $url >> check_sendsms.log 2>&1
+
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
+   [ 1 -ne `grep -c '<Authorization failed for sendsms>' \
+       check_sendsms_sms.log` ]
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
+#Password. Ditto.
+url="http://localhost:$sendsmsport/cgi-bin/sendsms?from=123&to=234&\
+text=&username=$username&password="
+
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null ||
+   [ 1 -ne `grep -c '<Authorization failed for sendsms>' \
+       check_sendsms_sms.log` ]
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
+kill -INT $bbpid
+wait
+
+# Do we panic when going down ?
+if grep 'WARNING:|ERROR:|PANIC:' check_sendsms*.log >/dev/null
+then
+	echo check_sendsms.sh failed 1>&2
+	echo See check_sendsms*.log for info 1>&2
+	exit 1
+fi
+
 rm check_sendsms*.log
+
+exit 0
+
+
+
+
