578d36d5d554f6b1cfa71008e8f1dfe3bc354c6f
stolj <stolj@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2002-05-05 22:26:40 +0000

    * checks/check_http.sh: added SSL-enabled HTTP server and client checks
    git-svn-id: https://svn.kannel.org/gateway/trunk@2814 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- checks/check_http.sh
+++ checks/check_http.sh
@@ -1,32 +1,50 @@
-#!/bin/sh
-#
-# Use `test/test_http{,_server}' to test gwlib/http.c.
-
-set -e
-
-times=2
-port="8040"
-url="http://localhost:$port/foo.txt"
-quiturl="http://localhost:$port/quit"
-loglevel=0
-
-test/test_http_server -p $port -v $loglevel > check_http_server.log 2>&1 &
-serverpid=$!
-
-sleep 1
-
-test/test_http -r $times $url > check_http.log 2>&1
+#!/bin/sh 
+# 
+# Use `test/test_http{,_server}' to test gwlib/http.c. 
+# Incuding the SSL client and server componentes. 
+ 
+set -e 
+ 
+times=2 
+port="8040" 
+port_ssl="8041" 
+url="http://localhost:$port/foo.txt" 
+url_ssl="https://localhost:$port_ssl/foo.txt" 
+quiturl="http://localhost:$port/quit" 
+quiturl_ssl="https://localhost:$port_ssl/quit" 
+ssl_cert="gw/cert.pem" 
+ssl_key="gw/key.pem" 
+ssl_clientcert="/tmp/clientcert.pem" 
+loglevel=0 
+ 
+cat $ssl_cert $ssl_key > $ssl_clientcert 
+ 
+test/test_http_server -p $port -v $loglevel > check_http_server.log 2>&1 & 
+serverpid=$! 
+ 
+sleep 1 
+ 
+test/test_http_server -p $port_ssl -v $loglevel -s -c $ssl_cert -k $ssl_key > check_https_server.log 2>&1 & 
+serverpid_ssl=$! 
+ 
+sleep 1 
+ 
+test/test_http -r $times $url > check_http.log 2>&1 
 ret=$?
 
+test/test_http -r $times -s -c $ssl_clientcert $url_ssl > check_https.log 2>&1 
+ret=$? 
+ 
 test/test_http -r 1 $quiturl >> check_http.log 2>&1
+test/test_http -r 1 -s -c $ssl_clientcert $quiturl_ssl >> check_https.log 2>&1 
 wait
 
-if [ "$ret" != 0 ] || \
-   grep ERROR: check_http.log check_http_server.log > /dev/null 
-then
-	echo check_http failed 1>&2
-	echo See check_http.log and check_http_server.log for info 1>&2
-	exit 1
-fi
+if grep 'ERROR:|PANIC:' check_http*.log check_http*_server.log  > /dev/null  
+then 
+	echo check_http failed 1>&2 
+	echo See check_http[s].log and check_http[s]_server.log for info 1>&2 
+	exit 1 
+fi 
 
-rm check_http.log check_http_server.log
+rm check_http*.log
+rm $ssl_clientcert
