9db6100a7fefb72581bb97d05c4b8ea95b612b92
stolj <stolj@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2003-04-04 11:51:35 +0000

    * gw/smsc/smsc_http.c: fixed a number of bugs. Including bearerbox crash       if smsc-id is stopped and /status page is called. Restructured IO threads       to work cleanly now, which means don't 'fail' a MT message when the HTTP       server can not be triggered. Instead use 'reconnect-delay' to try again.       Aded also another 'system-type = brunet' for an implemenation of Brunet's       HTTP interface.
    git-svn-id: https://svn.kannel.org/gateway/trunk@3519 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/smsc/smsc_http.c
+++ gw/smsc/smsc_http.c
@@ -14,12 +14,10 @@
  * o For MT messages, Kannel does HTTP GET or POST to given address, in format
  *   defined by type of HTTP based protocol
  *
- *
  * The 'type' of requests and replies are defined by 'system-type' variable.
  * The only type of HTTP requests currently supported are basic Kannel.
  * If new support is added, smsc_http_create is modified accordingly and new
  * functions added.
- *
  *
  *
  * KANNEL->KANNEL linking: (UDH not supported in MO messages)
@@ -51,9 +49,8 @@
  *  username = XXX
  *  password = YYY
  *  
- *
- *
  * Kalle Marjola for Project Kannel 2001
+ * Stipe Tolj <tolj@wapme-systems.de>
  */
 
 #include <sys/types.h>
@@ -71,36 +68,35 @@
 
 
 typedef struct conndata {
-    HTTPCaller  *http_ref;
-    long	receive_thread;
-    long	send_cb_thread;
-    int		shutdown;
-    int		port;	      	/* port for receiving SMS'es */
-    Octstr	*allow_ip;
-    Octstr	*send_url;
-    long	open_sends;
-    Octstr	*username;	/* if needed */
-    Octstr	*password;	/* as said */
-    int         no_sender;      /* ditto */
-    int         no_coding;      /* this, too */
-    int         no_sep;         /* not to mention this */
+    HTTPCaller *http_ref;
+    long receive_thread;
+    long send_cb_thread;
+    int shutdown;
+    int	port;   /* port for receiving SMS'es */
+    Octstr *allow_ip;
+    Octstr *send_url;
+    long open_sends;
+    Octstr *username;   /* if needed */
+    Octstr *password;   /* as said */
+    int no_sender;      /* ditto */
+    int no_coding;      /* this, too */
+    int no_sep;         /* not to mention this */
 
     /* callback functions set by HTTP-SMSC type */
-
     void (*send_sms) (SMSCConn *conn, Msg *msg);
     void (*parse_reply) (SMSCConn *conn, Msg *msg, int status,
-			 List *headers, Octstr *body);
+                         List *headers, Octstr *body);
     void (*receive_sms) (SMSCConn *conn, HTTPClient *client,
-			 List *headers, Octstr *body, List *cgivars);
+                         List *headers, Octstr *body, List *cgivars);
 } ConnData;
 
 
 static void conndata_destroy(ConnData *conndata)
 {
     if (conndata == NULL)
-	return;
+        return;
     if (conndata->http_ref)
-	http_caller_destroy(conndata->http_ref);
+        http_caller_destroy(conndata->http_ref);
     octstr_destroy(conndata->allow_ip);
     octstr_destroy(conndata->send_url);
     octstr_destroy(conndata->username);
@@ -111,7 +107,7 @@ static void conndata_destroy(ConnData *conndata)
 
 
 /*
- * thread to listen to HTTP requests from other end
+ * Thread to listen to HTTP requests from SMSC entity
  */
 static void httpsmsc_receiver(void *arg)
 {
@@ -124,42 +120,45 @@ static void httpsmsc_receiver(void *arg)
     /* Make sure we log into our own log-file if defined */
     log_thread_to(conn->log_idx);
  
-    while(conndata->shutdown == 0) {
+    while (conndata->shutdown == 0) {
 
-	/* XXX if conn->is_stopped, do not receive new messages.. */
+        /* XXX if conn->is_stopped, do not receive new messages.. */
 	
-	client = http_accept_request(conndata->port, &ip, &url,
-				     &headers, &body, &cgivars);
-	if (client == NULL)
-	    break;
+        client = http_accept_request(conndata->port, &ip, &url,
+                                     &headers, &body, &cgivars);
+        if (client == NULL)
+            break;
 
-	debug("smsc.http", 0, "Got request '%s'", octstr_get_cstr(url));
+        debug("smsc.http", 0, "Got request `%s'", octstr_get_cstr(url));
 
-	if (connect_denied(conndata->allow_ip, ip)) {
-	    info(0, "httpsmsc: connection '%s' tried from denied "
-		 "host %s, ignored", octstr_get_cstr(url),
-		 octstr_get_cstr(ip));
-	    http_close_client(client);
-	} else
-	    conndata->receive_sms(conn, client, headers, body, cgivars);
+        if (connect_denied(conndata->allow_ip, ip)) {
+            info(0, "httpsmsc: connection `%s' tried from denied "
+                    "host %s, ignored", octstr_get_cstr(url),
+                    octstr_get_cstr(ip));
+            http_close_client(client);
+        } else
+            conndata->receive_sms(conn, client, headers, body, cgivars);
 
-	debug("smsc.http", 0, "destroying client information");
-	octstr_destroy(url);
-	octstr_destroy(ip);
-	octstr_destroy(body);
-	http_destroy_headers(headers);
-	http_destroy_cgiargs(cgivars);
+        debug("smsc.http", 0, "destroying client information");
+        octstr_destroy(url);
+        octstr_destroy(ip);
+        octstr_destroy(body);
+        http_destroy_headers(headers);
+        http_destroy_cgiargs(cgivars);
     }
-    debug("http_smsc", 0, "httpsmsc_receiver dying");
+    debug("smsc.http", 0, "httpsmsc_receiver dying");
 
     conndata->shutdown = 1;
     http_close_port(conndata->port);
-    http_caller_signal_shutdown(conndata->http_ref);
+    
+    /* unblock http_receive_result() if there are no open sends */
+    if (conndata->open_sends == 0)
+        http_caller_signal_shutdown(conndata->http_ref);
 }
 
 
 /*
- *   thread to handle finished sendings
+ * Thread to handle finished sendings
  */
 static void httpsmsc_send_cb(void *arg)
 {
@@ -173,23 +172,52 @@ static void httpsmsc_send_cb(void *arg)
     /* Make sure we log into our own log-file if defined */
     log_thread_to(conn->log_idx);
 
-    while(conndata->shutdown == 0 || conndata->open_sends) {
+    while (conndata->shutdown == 0 || conndata->open_sends) {
 
-	msg = http_receive_result(conndata->http_ref, &status,
-				 &final_url, &headers, &body);
-	if (msg == NULL)
-	    break;	/* they told us to die */
+        msg = http_receive_result(conndata->http_ref, &status,
+                                  &final_url, &headers, &body);
 
-	conndata->open_sends--;
+        if (msg == NULL)
+            break;  /* they told us to die, by unlocking */
 
-	conndata->parse_reply(conn, msg, status, headers, body);
+        /* Handle various states here. */
 
-	http_destroy_headers(headers);
-	octstr_destroy(final_url);
-	octstr_destroy(body);
+        /* request failed and we are not in shutdown mode */
+        if (status == -1 && conndata->shutdown == 0) { 
+            error(0, "HTTP[%s]: Couldn't connect to SMS center "
+                     "(retrying in %ld seconds).",
+                     octstr_get_cstr(conn->id), conn->reconnect_delay);
+            conn->status = SMSCCONN_RECONNECTING; 
+            gwthread_sleep(conn->reconnect_delay);
+            debug("smsc.http.kannel", 0, "re-sending request");
+            conndata->send_sms(conn, msg);
+            continue; 
+        } 
+        /* request failed and we *are* in shutdown mode, drop the message */ 
+        else if (status == -1 && conndata->shutdown == 1) {
+        }
+        /* request succeeded */    
+        else {
+            /* we received a response, so this link is considered online again */
+            if (status && conn->status != SMSCCONN_ACTIVE) {
+                conn->status = SMSCCONN_ACTIVE;
+            }
+            conndata->parse_reply(conn, msg, status, headers, body);
+        }
+   
+        conndata->open_sends--;
+
+        http_destroy_headers(headers);
+        octstr_destroy(final_url);
+        octstr_destroy(body);
     }
-    debug("http-smsc", 0, "httpsmsc_send_cb dying");
+    debug("smsc.http", 0, "httpsmsc_send_cb dying");
     conndata->shutdown = 1;
+
+    if (conndata->open_sends) {
+        warning(0, "HTTP[%s]: Shutdown while <%d> requests are pending.",
+                octstr_get_cstr(conn->id), conndata->open_sends);
+    }
 
     gwthread_join(conndata->receive_thread);
 
@@ -215,7 +243,7 @@ static void httpsmsc_send_cb(void *arg)
  *   and must call appropriate bb_smscconn functions
  */
 
-/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+/*----------------------------------------------------------------
  * Kannel
  */
 
@@ -334,9 +362,9 @@ static void kannel_receive_sms(SMSCConn *conn, HTTPClient *client,
     }
     debug("smsc.http.kannel", 0, "Received an HTTP request");
     
-    if (   user == NULL || pass == NULL
-	   || octstr_compare(user, conndata->username)!= 0
-	   || octstr_compare(pass, conndata->password)!= 0) {
+    if (user == NULL || pass == NULL ||
+	    octstr_compare(user, conndata->username) != 0 ||
+	    octstr_compare(pass, conndata->password) != 0) {
 
 	debug("smsc.http.kannel", 0, "Authorization failure");
 	retmsg = octstr_create("Authorization failed for sendsms");
@@ -381,11 +409,173 @@ static void kannel_receive_sms(SMSCConn *conn, HTTPClient *client,
 }
 
 
-/*xxxxxxxxxxxxxxxxxxxxxxx
+/*----------------------------------------------------------------
+ * Brunet - A german aggregator (mainly doing T-Mobil D1 connections)
  *
- * ADD NEW CONTENT GATEWAY/HTTP SMSC CALLBACKS HERE
+ *  o bruHTT v1.3L (for MO traffic) 
+ *  o bruHTP v1.8L (for MT traffic)
+ *
+ * Stipe Tolj <tolj@wapme-systems.de>
  */
 
+/* MT related function */
+static void brunet_send_sms(SMSCConn *conn, Msg *sms)
+{
+    ConnData *conndata = conn->data;
+    Octstr *url, *tid;
+    List *headers;
+
+    /* 
+     * Construct TransactionId like this: <timestamp>-<receiver msisdn>-<msg.id> 
+     * to garantee uniqueness. 
+     */
+    tid = octstr_format("%d-%S-%d", time(NULL), sms->sms.receiver, sms->sms.id);
+
+    /* format the URL for call */
+    url = octstr_format("%S?"
+        "CustomerId=%E&MsIsdn=%E&Originator=%E&MessageType=%E"
+        "&Text=%E&TransactionId=%E"
+        "&SMSCount=1&ActionType=A&ServiceDeliveryType=P", /* static parts */
+        conndata->send_url,
+        conndata->username, sms->sms.receiver, sms->sms.sender,
+        (octstr_len(sms->sms.udhdata) ? octstr_imm("B") : octstr_imm("S")),
+        sms->sms.msgdata, tid);
+
+    if (octstr_len(sms->sms.udhdata)) {
+        octstr_format_append(url, "&XSer=%E", sms->sms.udhdata);
+    }
+
+    headers = list_create();
+    debug("smsc.http.brunet", 0, "HTTP[%s]: Sending request",
+          octstr_get_cstr(conn->id));
+
+    /* 
+     * Brunet requires an SSL-enabled HTTP client call, this is handled
+     * transparently by the Kannel HTTP layer module.
+     */
+    http_start_request(conndata->http_ref, HTTP_METHOD_GET, url, headers, 
+                       NULL, 0, sms, NULL);
+
+    octstr_destroy(url);
+    octstr_destroy(tid);
+    http_destroy_headers(headers);
+}
+
+static void brunet_parse_reply(SMSCConn *conn, Msg *msg, int status,
+                               List *headers, Octstr *body)
+{
+    if (status == HTTP_OK || status == HTTP_ACCEPTED) {
+        if (octstr_case_compare(body, octstr_imm("Status=0")) == 0) {
+            bb_smscconn_sent(conn, msg);
+        } else {
+            error(0, "HTTP[%s]: Message was malformed. SMSC response `%s'.", 
+                  octstr_get_cstr(conn->id), octstr_get_cstr(body));
+            bb_smscconn_send_failed(conn, msg, SMSCCONN_FAILED_MALFORMED);
+        }
+    } else {
+        error(0, "HTTP[%s]: Message was rejected. SMSC reponse `%s'.", 
+              octstr_get_cstr(conn->id), octstr_get_cstr(body));
+        bb_smscconn_send_failed(conn, msg, SMSCCONN_FAILED_REJECTED);
+    }
+}
+
+/* MO related function */
+static void brunet_receive_sms(SMSCConn *conn, HTTPClient *client,
+                               List *headers, Octstr *body, List *cgivars)
+{
+    ConnData *conndata = conn->data;
+    Octstr *user, *from, *to, *text, *udh, *date, *type;
+    Octstr *retmsg;
+    int	mclass, mwi, coding, validity, deferred; 
+    List *reply_headers;
+    int ret;
+
+    mclass = mwi = coding = validity = deferred = 0;
+
+    user = http_cgi_variable(cgivars, "CustomerId");
+    from = http_cgi_variable(cgivars, "MsIsdn");
+    to = http_cgi_variable(cgivars, "Recipient");
+    text = http_cgi_variable(cgivars, "SMMO");
+    udh = http_cgi_variable(cgivars, "XSer");
+    date = http_cgi_variable(cgivars, "DateReceived");
+    type = http_cgi_variable(cgivars, "MessageType");
+
+    /*
+    tmp_string = http_cgi_variable(cgivars, "flash");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &mclass);
+    }
+    tmp_string = http_cgi_variable(cgivars, "mclass");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &mclass);
+    }
+    tmp_string = http_cgi_variable(cgivars, "mwi");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &mwi);
+    }
+    tmp_string = http_cgi_variable(cgivars, "coding");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &coding);
+    }
+    tmp_string = http_cgi_variable(cgivars, "validity");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &validity);
+    }
+    tmp_string = http_cgi_variable(cgivars, "deferred");
+    if(tmp_string) {
+	sscanf(octstr_get_cstr(tmp_string),"%d", &deferred);
+    }
+    */
+
+    debug("smsc.http.brunet", 0, "HTTP[%s]: Received a request",
+          octstr_get_cstr(conn->id));
+    
+    if (user == NULL || octstr_compare(user, conndata->username) != 0) {
+        error(0, "HTTP[%s]: Authorization failure. CustomerId was <%s>.",
+              octstr_get_cstr(conn->id), octstr_get_cstr(user));
+        retmsg = octstr_create("Authorization failed for MO submission.");
+    }
+    else if (from == NULL || to == NULL || text == NULL) {
+        error(0, "HTTP[%s]: Insufficient args.",
+              octstr_get_cstr(conn->id));
+        retmsg = octstr_create("Insufficient arguments, rejected.");
+    }
+    else {
+        Msg *msg;
+        msg = msg_create(sms);
+
+        debug("smsc.http.brunet", 0, "HTTP[%s]: Received new MO SMS.",
+              octstr_get_cstr(conn->id));
+	
+        msg->sms.sender = octstr_duplicate(from);
+        msg->sms.receiver = octstr_duplicate(to);
+        msg->sms.msgdata = octstr_duplicate(text);
+        msg->sms.udhdata = octstr_duplicate(udh);
+
+        msg->sms.smsc_id = octstr_duplicate(conn->id);
+        msg->sms.time = time(NULL); /* XXX maybe extract from DateReceived */ 
+        msg->sms.mclass = mclass;
+        msg->sms.mwi = mwi;
+        msg->sms.coding = coding;
+        msg->sms.validity = validity;
+        msg->sms.deferred = deferred;
+
+        ret = bb_smscconn_receive(conn, msg);
+        if (ret == -1)
+            retmsg = octstr_create("Status=1");
+        else
+            retmsg = octstr_create("Status=0");
+    }
+
+    reply_headers = list_create();
+    http_header_add(reply_headers, "Content-Type", "text/plain");
+    debug("smsc.http.brunet", 0, "HTTP[%s]: Sending reply `%s'.",
+          octstr_get_cstr(conn->id), octstr_get_cstr(retmsg));
+    http_send_reply(client, HTTP_OK, reply_headers, retmsg);
+
+    octstr_destroy(retmsg);
+    http_destroy_headers(reply_headers);
+}
 
 
 
@@ -408,7 +598,9 @@ static int httpsmsc_send(SMSCConn *conn, Msg *msg)
 static long httpsmsc_queued(SMSCConn *conn)
 {
     ConnData *conndata = conn->data;
-    return conndata->open_sends;
+
+    return (conndata ? (conn->status != SMSCCONN_DEAD ? 
+            conndata->open_sends : 0) : 0);
 }
 
 
@@ -433,13 +625,13 @@ int smsc_http_create(SMSCConn *conn, CfgGroup *cfg)
     int ssl = 0;   /* indicate if SSL-enabled server should be used */
 
     if (cfg_get_integer(&portno, cfg, octstr_imm("port")) == -1) {
-	error(0, "'port' invalid in smsc 'http' record.");
-	return -1;
+        error(0, "'port' invalid in smsc 'http' record.");
+        return -1;
     }
     if ((type = cfg_get(cfg, octstr_imm("system-type")))==NULL) {
-	error(0, "'type' missing in smsc 'http' record.");
-	octstr_destroy(type);
-	return -1;
+        error(0, "'type' missing in smsc 'http' record.");
+        octstr_destroy(type);
+        return -1;
     }
     conndata = gw_malloc(sizeof(ConnData));
     conndata->http_ref = NULL;
@@ -453,16 +645,25 @@ int smsc_http_create(SMSCConn *conn, CfgGroup *cfg)
     cfg_get_bool(&conndata->no_sep, cfg, octstr_imm("no-sep"));
 
     if (conndata->send_url == NULL)
-	panic(0, "Sending not allowed");
+        panic(0, "Sending not allowed");
 
-    if (octstr_case_compare(type, octstr_imm("kannel"))==0) {
-	if (conndata->username == NULL || conndata->password == NULL) {
-	    error(0, "username and password required for Kannel http smsc");
-	    goto error;
-	}
-	conndata->receive_sms = kannel_receive_sms;
-	conndata->send_sms = kannel_send_sms;
-	conndata->parse_reply = kannel_parse_reply;
+    if (octstr_case_compare(type, octstr_imm("kannel")) == 0) {
+        if (conndata->username == NULL || conndata->password == NULL) {
+            error(0, "username and password required for Kannel http smsc");
+            goto error;
+        }
+        conndata->receive_sms = kannel_receive_sms;
+        conndata->send_sms = kannel_send_sms;
+        conndata->parse_reply = kannel_parse_reply;
+    }
+    else if (octstr_case_compare(type, octstr_imm("brunet")) == 0) {
+        if (conndata->username == NULL) {
+            error(0, "username required for Brunet http smsc");
+            goto error;
+        }
+        conndata->receive_sms = brunet_receive_sms;
+        conndata->send_sms = brunet_send_sms;
+        conndata->parse_reply = brunet_parse_reply;
     }
     /*
      * ADD NEW HTTP SMSC TYPES HERE
@@ -514,12 +715,4 @@ error:
     octstr_destroy(type);
     return -1;
 }
-
-
-
-
-
-
-
-
 
