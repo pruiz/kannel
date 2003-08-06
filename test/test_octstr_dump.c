2d04b20a07fffc1a213dedeeb2ef928a07c4f5ea
stolj <stolj@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2003-08-06 17:09:54 +0000

    * test/test_ocstr_dump.c: added automatic convertion from hex values in       text/plain files to be converted to binary before cumping the content.
    git-svn-id: https://svn.kannel.org/gateway/trunk@3766 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- test/test_octstr_dump.c
+++ test/test_octstr_dump.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv)
 {
-    Octstr *data, *filename;
+    Octstr *data, *filename, *hex;
 
     gwlib_init();
 
@@ -21,10 +21,30 @@ int main(int argc, char **argv)
     filename = octstr_create(argv[1]);
     data = octstr_read_file(octstr_get_cstr(filename));
 
+    /* 
+     * We test if this is a text/plain file with hex values in it.
+     * Therefore copy the data and trail off any CR and LF from 
+     * beginning and end and test if the result is only hex chars.
+     * If yes, then convert to binary before dumping.
+     */
+    hex = octstr_duplicate(data);
+    octstr_strip_crlfs(hex);
+    if (octstr_is_all_hex(hex)) {
+        debug("",0,"Trying to converting from hex to binary.");
+        if (octstr_hex_to_binary(hex) == 0) {
+            debug("",0,"Convertion was successfull.");
+            octstr_destroy(data);
+            data = octstr_duplicate(hex);
+        } else {
+            debug("",0,"Failed to convert from hex?!");
+        }
+    }                                      
+
     debug("",0,"Dumping file `%s':", octstr_get_cstr(filename));
     octstr_dump(data, 0);
 
     octstr_destroy(data);
+    octstr_destroy(hex);
     gwlib_shutdown();
     return 0;
 }
