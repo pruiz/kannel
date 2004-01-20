703cde75b5e07e9edd0d8cb2e64f45035571ed1b
amalysh <amalysh@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2004-01-20 18:48:07 +0000

    * gw/shared.c: added more info to 'version_report_string'.       Compiler,Build time, Mysql build with version, running with.
    git-svn-id: https://svn.kannel.org/gateway/trunk@3995 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/shared.c
+++ gw/shared.c
@@ -73,6 +73,7 @@
 #endif
 #ifdef HAVE_MYSQL 
 #include <mysql_version.h>
+#include <mysql.h>
 #endif
 #ifdef HAVE_SQLITE 
 #include <sqlite.h>
@@ -97,7 +98,7 @@ Octstr *version_report_string(const char *boxname)
     struct utsname u;
 
     uname(&u);
-    return octstr_format(GW_NAME " %s version `%s'.\n"
+    return octstr_format(GW_NAME " %s version `%s'.\nBuild `%s', compiler `%s'.\n"
     	    	    	 "System %s, release %s, version %s, machine %s.\n"
 			 "Hostname %s, IP %s.\n"
 			 "Libxml version %s.\n"
@@ -109,7 +110,7 @@ Octstr *version_report_string(const char *boxname)
              "%s.\n"
 #endif
 #ifdef HAVE_MYSQL
-             "Using MySQL %s.\n"
+             "Compiled with MySQL %s, using MySQL %s.\n"
 #endif
 #ifdef HAVE_SDB
              "Using LibSDB %s.\n"
@@ -119,15 +120,21 @@ Octstr *version_report_string(const char *boxname)
 #endif
              "Using %s malloc.\n",
 			 boxname, GW_VERSION,
+#ifdef __GNUC__ 
+             (__DATE__ " " __TIME__) ,
+             __VERSION__,
+#else 
+             "unknown" , "unknown"
+#endif 
 			 u.sysname, u.release, u.version, u.machine,
 			 octstr_get_cstr(get_official_name()),
 			 octstr_get_cstr(get_official_ip()),
-			 LIBXML_VERSION_STRING,
+			 LIBXML_DOTTED_VERSION,
 #ifdef HAVE_LIBSSL
              OPENSSL_VERSION_TEXT,
 #endif
 #ifdef HAVE_MYSQL
-             MYSQL_SERVER_VERSION,
+             MYSQL_SERVER_VERSION, mysql_get_client_info(),
 #endif
 #ifdef HAVE_SDB
              LIBSDB_VERSION,
