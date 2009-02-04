73b0a3744b73e581dab7f7d07115fdcf727792b8
stolj <stolj@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2009-02-04 15:16:37 +0000

    2009-02-04  Stipe Tolj  <stolj at kannel.org>     * test/test_octstr_format.c: fix intendion according to our CodingStyle       policy. NLC.
    git-svn-id: https://svn.kannel.org/gateway/trunk@4689 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- test/test_octstr_format.c
+++ test/test_octstr_format.c
@@ -60,38 +60,39 @@
 
 #include "gwlib/gwlib.h"
 
-int main(void) {
-	Octstr *os, *os2, *os3, *os4, *os5;
-        unsigned long long n = 78234782386423784267234;
+int main(void) 
+{
+    Octstr *os, *os2, *os3, *os4, *os5;
+    unsigned long long n = 78234782386423784267234;
 
-	gwlib_init();
+    gwlib_init();
 
-	os = octstr_format("hi, %% %-5.*s, <%*s>, %-5d + %05d = %d, -%5.2f",
-			   3, "world", 3, "", 1, 2, 3, 3.1415927);
-	octstr_dump(os, 0);
+    os = octstr_format("hi, %% %-5.*s, <%*s>, %-5d + %05d = %d, -%5.2f",
+                       3, "world", 3, "", 1, 2, 3, 3.1415927);
+    octstr_dump(os, 0);
 	
-	os2 = octstr_format("<%S>", os);
-	octstr_dump(os2, 0);
+    os2 = octstr_format("<%S>", os);
+    octstr_dump(os2, 0);
 	
-	octstr_format_append(os2, "yeehaa!");
-	octstr_dump(os2, 0);
+    octstr_format_append(os2, "yeehaa!");
+    octstr_dump(os2, 0);
 	
-	os3 = octstr_format("NULL=%p &os=%p", (void *) NULL, (void *) &os);
-	octstr_dump(os3, 0);
+    os3 = octstr_format("NULL=%p &os=%p", (void *) NULL, (void *) &os);
+    octstr_dump(os3, 0);
 	
-	os4 = octstr_format("Encode %E and limited %-10.10E", os, os);
-	octstr_dump(os4, 0);
+    os4 = octstr_format("Encode %E and limited %-10.10E", os, os);
+    octstr_dump(os4, 0);
 
-        os5 = octstr_format("Encode %%llu: %llu", n);
-        octstr_dump(os5, 0);
+    os5 = octstr_format("Encode %%llu: %llu", n);
+    octstr_dump(os5, 0);
 
-	octstr_destroy(os);
-	octstr_destroy(os2);
-	octstr_destroy(os3);
-        octstr_destroy(os4);
-        octstr_destroy(os5);
+    octstr_destroy(os);
+    octstr_destroy(os2);
+    octstr_destroy(os3);
+    octstr_destroy(os4);
+    octstr_destroy(os5);
 	
-	gwlib_shutdown();
+    gwlib_shutdown();
 	
 	return 0;
 }
