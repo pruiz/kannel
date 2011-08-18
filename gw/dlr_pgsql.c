8a242e8e1e1655d729d91260befdf178cad2a40b
amalysh <amalysh@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2011-08-18 10:07:47 +0000

    * gw/dlr_pgsql.c: fixed panic when DB is not available.       Thanks to Alan McNatty <alan at catalyst.net.nz>
    git-svn-id: https://svn.kannel.org/gateway/trunk@4918 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/dlr_pgsql.c
+++ gw/dlr_pgsql.c
@@ -185,8 +185,6 @@ static struct dlr_entry *dlr_pgsql_get(const Octstr *smsc, const Octstr *ts, con
 
     if (result == NULL || gwlist_len(result) < 1) {
         debug("dlr.pgsql", 0, "no rows found");
-        while((row = gwlist_extract_first(result)))
-            gwlist_destroy(row, octstr_destroy_item);
         gwlist_destroy(result, NULL);
         return NULL;
     }
@@ -280,9 +278,9 @@ static long dlr_pgsql_messages(void)
         ret = -1;
     } else {
         ret = atol(octstr_get_cstr(gwlist_get(gwlist_get(res, 0), 0)));
+        gwlist_destroy(gwlist_extract_first(res), octstr_destroy_item);
     }
 
-    gwlist_destroy(gwlist_extract_first(res), octstr_destroy_item);
     gwlist_destroy(res, NULL);
         
     return ret;
